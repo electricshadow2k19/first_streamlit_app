1e266ba9f1e3adfa39fe0c551c87e21fdcd6e85f
electricshadow2k19 <104021674+electricshadow2k19@users.noreply.github.com>
2022-04-19 12:49:13 -0400

    Update streamlit_app.py
    Edited breakfast menu > Breakfast Favourites
    Added new header for Build your own smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,11 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favourites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard boiled eggs')
+
+
+streamlit.header('Build your own smoothie')
 
