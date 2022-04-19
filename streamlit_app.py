672127374e9dfbed5ddf7ec0f4e134a85d710885
electricshadow2k19 <104021674+electricshadow2k19@users.noreply.github.com>
2022-04-19 12:55:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,4 +13,4 @@ streamlit.header('Build your own smoothie')
 
 my_fruits_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlist.dataframe(my_fruits_list)
+streamlit.dataframe(my_fruits_list)
